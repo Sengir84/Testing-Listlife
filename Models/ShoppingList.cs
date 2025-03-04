db7c42f11ee83d3b0b115cb669f26f505732e4ea
NanEkman <nen_92@hotmail.com>
2025-03-04 13:47:12 +0100

    add shopinglist

--- Models/ShoppingList.cs
+++ Models/ShoppingList.cs
@@ -4,8 +4,12 @@
     {
         public int Id { get; set; }
         public string Product { get; set; }
-        public string Category { get; set; } // SELECT
+        public string Category { get; set; } 
         public string Amount { get; set; }
+
+        // ConnectionTable
+        public string UserListId { get; set; }
+        //public UserList UserList { get; set; }
 
     }
 }
--- Pages/MyPage.cshtml.cs
+++ Pages/MyPage.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ListLife.Pages
 {
+    //[Authorize]
     public class MyPage : PageModel
     {
         public void OnGet()
