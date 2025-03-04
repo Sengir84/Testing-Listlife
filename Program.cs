24cc1f388c89a9761ca472b670e1696445c99ea6
HP <farishtasulaiman@gmail.com>
2025-03-04 10:14:55 +0100

    tog tillbaka kommentaren

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
