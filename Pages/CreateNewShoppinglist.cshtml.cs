03ce35e7c8ec0d27d9b6f610a38d72177a213a29
NanEkman <nen_92@hotmail.com>
2025-03-05 13:34:22 +0100

    able to redirect from mypage to cretenewshoppinlist

--- Pages/CreateNewShoppinglist.cshtml.cs
+++ Pages/CreateNewShoppinglist.cshtml.cs
@@ -14,36 +14,44 @@ namespace ListLife.Pages
         private readonly ApplicationDbContext context;
 
         // Hanterar användare via ASP.NET Identity (UserManager) för att hämta och hantera inloggade användare
-        private readonly UserManager<IdentityUser> userManager;
+        private readonly UserManager<UserList> userManager;
 
 
         // Property för att hålla användarens shoppinglistor
         public IList<ShoppingList> ShoppingLists { get; set; }
 
         // Konstruktor
-        public CreateNewShoppingList(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        public CreateNewShoppingList(ApplicationDbContext context, UserManager<UserList> userManager)
         {
             this.context = context;
             this.userManager = userManager;
         }
 
-        public async Task<IActionResult> OnGetAsync()
+        //public async Task<IActionResult> OnGetAsync()
+        //{
+        //    // Hämta den inloggade användaren asynkront
+        //    //var user = await userManager.GetUserAsync(User); // User är en inbyggd property i PageModel som innehåller information om den inloggade användaren
+
+        //    //if (user != null)
+        //    //{
+        //    //    // Hämta alla shoppinglistor för den inloggade användaren
+        //    //    ShoppingLists = context.ShoppingLists
+        //    //        .Where(shoppingList => shoppingList.UserId == user.Id)  // Filtrera baserat på användarens ID
+        //    //        .ToList();
+
+        //    //    List<ShoppingList> shoppingLists = new();
+        //    //    context.Add(shoppingLists);
+
+        //    //    //context.Add(ShoppingLists); // FEL? 
+        //    //    await context.SaveChangesAsync();                
+        //    //}
+
+        //    //return RedirectToPage("/MyPage");
+        //}
+
+        public void OnGet()
         {
-            // Hämta den inloggade användaren asynkront
-            var user = await userManager.GetUserAsync(User); // User är en inbyggd property i PageModel som innehåller information om den inloggade användaren
 
-            if (user != null)
-            {
-                // Hämta alla shoppinglistor för den inloggade användaren
-                ShoppingLists = context.ShoppingLists
-                    .Where(shoppingList => shoppingList.UserId == user.Id)  // Filtrera baserat på användarens ID
-                    .ToList();
-
-                context.Add(ShoppingLists); // FEL? 
-                await context.SaveChangesAsync();                
-            }
-
-            return RedirectToPage("/MyPage");
         }
     }
 }
