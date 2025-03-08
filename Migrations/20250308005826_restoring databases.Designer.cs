e266128f9ca1bdb51f31ad1d55f2d7e1d36d901d
Alexander Larsson <alexander.larsson@edu.newton.se>
2025-03-08 02:05:26 +0100

    restoring databases

--- Migrations/20250308005826_restoring databases.Designer.cs
+++ Migrations/20250308005826_restoring databases.Designer.cs
@@ -4,6 +4,7 @@ using ListLife.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ListLife.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250308005826_restoring databases")]
+    partial class restoringdatabases
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
