dadbf35c488b4d1c324188c20793ce8e1a271b74
NanEkman <nen_92@hotmail.com>
2025-03-16 13:27:10 +0100

    Removed decimal code in DbContext and Changed Database Product Model from decimal to int

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,7 +5,7 @@
         // PK för produkten
         public int Id { get; set; }  
         public string Name { get; set; }  
-        public decimal Amount { get; set; } 
+        public int Amount { get; set; } 
         public string Category { get; set; } 
 
         // FK till ShoppingList
