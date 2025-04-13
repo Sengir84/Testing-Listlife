3fad1991189724761bcdabcf002f96e12dfff748
Alexander Larsson <alexander.larsson@edu.newton.se>
2025-04-13 22:12:55 +0200

    refaktorering för test

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,8 +4,19 @@
     {
         // PK for product
         public int Id { get; set; }  
-        public string Name { get; set; }  
-        public int Amount { get; set; } 
+        public string Name { get; set; }
+        
+        private int _amount;
+        public int Amount
+        {
+            get => _amount;
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentException("Amount cannot be negative");
+                _amount = value;
+            }
+        }
         public string Category { get; set; } 
 
         // FK to Shopping list
